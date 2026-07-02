ff21f0ad0868e4b5731ba3f0cc3aa5c816b007f1
emrecancc <emrecancerli55@gmail.com>
2026-07-02 23:46:19 +0300

    fix: update async-ops test to use async/await

--- src/__tests__/async-ops.test.js
+++ src/__tests__/async-ops.test.js
@@ -1,12 +1,9 @@
+const asyncOps = require('../async-ops');
+
 describe('batch processing', () => {
-  it('processes all items', (done) => {
-    const results = [];
-    const promises = Array.from({length: 10}, (_, i) =>
-      processItem(i).then(r => results.push(r))
-    );
-    setTimeout(() => {
-      expect(results).toHaveLength(10); // race condition: may not be done
-      done();
-    }, 100);
+  test('processes all items', async () => {
+    const items = Array.from({ length: 10 }, (_, i) => i);
+    const results = await Promise.all(items.map(item => asyncOps(item)));
+    expect(results).toHaveLength(10);
   });
 });
