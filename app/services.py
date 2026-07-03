39ac4f19ccf82ae9b04379f8299b158d8b680248
emrecancc <emrecancerli55@gmail.com>
2026-07-03 08:35:15 +0300

    ci: introduce syn_py (s19)

--- app/services.py
+++ app/services.py
@@ -1,8 +1,6 @@
-from app.models import User
-
 class UserService:
     def __init__(self, db):
         self.db = db
 
-    def get_user(self, user_id):
-        return self.db.query(User).filter(User.id == user_id).first()
+    def get_user(self, user_id)
+        return self.db.query('SELECT * FROM users WHERE id = ?', user_id)
